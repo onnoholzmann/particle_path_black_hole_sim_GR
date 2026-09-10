90873764db75e949ca015d785322c4d5bed2fe12
Onno Holzmann <143614975+onnoholzmann@users.noreply.github.com>
2026-09-10 15:45:34 +0200

    started the relative object class

--- calc_path.py
+++ calc_path.py
@@ -15,10 +15,11 @@ G needs to get converted into the right units
 
 # the attractor, like the sun or black hole
 class Attractor:
-  def __init__(self, mass, pos):
+  def __init__(self, mass, pos, spin_parameter):
     # the mass can also be noted like 5e10
     self.mass = mass
     self.pos = np.array(pos)
+    self.a = spin_parameter
 
 G_light = scipy.constants.G / scipy.constants.c**3
 
@@ -27,7 +28,7 @@ def step_newt(speed, pos, attractor_pos, mass, time_passed, dt=1):
   current_speed = speed.copy()
   current_pos = pos.copy()
 
-  for _ in range(time_passed):
+  for _ in range(0, time_passed, dt):
     r = np.sqrt(np.sum((current_pos-attractor_pos)**2))
     # direction_vector = (attractor_pos - current_pos)/r
     # acceleration = G_light * mass / r**2
@@ -60,4 +61,37 @@ class Newtonian_object:
 
       # print(self.pos)
     """
+    return self.pos
+
+
+@njit(fastmath=True)
+def step_relative(speed, pos, attractor_pos, mass, time_passed, dt=1):
+  current_speed = speed.copy()
+  current_pos = pos.copy()
+
+  for _ in range(0, time_passed, dt):
+    
+    current_pos += current_speed * dt
+
+class Relative_object:
+  def __init__(self, attractor, pos, speed):
+    # the pos and speed are in r, phi, theta, but keep theta=1/2*pi for now
+    self.attractor = attractor
+    self.pos = np.array(pos, dtype=float)
+    self.speed = np.array(speed, dtype=float)
+    self.E, self.L = self.calc_init_E_L()
+
+  def calc_init_E_L(self, is_prograde=True):
+    # this is in an ideal scenario of theta=1/2*pi and an stable circular orbit
+    factor = -1
+    if is_prograde:
+      factor = 1
+    r = np.sqrt(np.sum((self.pos-self.attractor.pos)**2))
+    E = (r**2 - 2*self.attractor.mass*r + factor*self.attractor.a*np.sqrt(self.attractor.mass)) / (r * np.sqrt(r**2 - 3*self.attractor.mass*r + factor*2*self.attractor.a*np.sqrt(self.attractor.mass*r)))
+    L = (factor*np.sqrt(self.attractor.mass)*(r**2 - factor*2*self.attractor.a*np.sqrt(self.attractor.mass*r) + self.attractor.a**2)) / (np.sqrt(r)*np.sqrt(r**2 - 3*self.attractor.mass*r + factor*2*self.attractor.a*r))
+
+    return E, L
+
+  def update(self, time_passed):
+    self.speed, self.pos = step_relative(self.speed, self.pos, self.attractor.pos, self.attractor.mass, time_passed)
     return self.pos
